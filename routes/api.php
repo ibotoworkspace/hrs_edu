044eb7339a6c5bdc4bddd5ceb2aec99fc3dfa9b8
AbubakarMian <AbubakarMian@github.com>
2021-02-19 14:37:57 +0500

    api routes updated

--- routes/api.php
+++ routes/api.php
@@ -23,14 +23,14 @@ Route::group(['middleware' => 'auth.client_token'], function () {
     // Route::post('video', 'Services\UserController@video');
 
     Route::post('user/signup', 'services\UserController@signUp');
-    Route::post('user/login', 'Services\UserController@login');
-    Route::post('user/forgetpassword', 'Services\UserController@forgetpassword');
-    Route::get('user/logout', 'Services\UserController@logout');
-    Route::get('user/home', 'Services\HomeController@home');
-    Route::get('user/registeredcourses', 'Services\CoursesController@registeredcourses');
-    Route::post('user/course', 'Services\CoursesController@course');
-    Route::post('user/quiz', 'Services\QuizControlller@quiz');
-    Route::post('user/chapters', 'Services\CoursesController@chapters');
-    Route::post('user/chapter', 'Services\CoursesController@chapter');
-    Route::post('user/contactus', 'Services\ContactusController@contactus');
+    Route::post('user/login', 'services\UserController@login');
+    Route::post('user/forgetpassword', 'services\UserController@forgetpassword');
+    Route::get('user/logout', 'services\UserController@logout');
+    Route::get('user/home', 'services\HomeController@home');
+    Route::get('user/registeredcourses', 'services\CoursesController@registeredcourses');
+    Route::post('user/course', 'services\CoursesController@course');
+    Route::post('user/quiz', 'services\QuizControlller@quiz');
+    Route::post('user/chapters', 'services\CoursesController@chapters');
+    Route::post('user/chapter', 'services\CoursesController@chapter');
+    Route::post('user/contactus', 'services\ContactusController@contactus');
 });
