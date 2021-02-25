7f110e69c823c5b92fda848e8c73ff0ef742206b
Muhammed Hamza <hamzamuhammed404@gmail.com>
2021-02-25 11:46:17 +0500

    migration

---
+++ database/migrations/2021_02_25_061016_alter_course_table_detail_datatype.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterCourseTableDetailDatatype extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->text('detail')->change();
+            $table->text('requirments')->change();
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
