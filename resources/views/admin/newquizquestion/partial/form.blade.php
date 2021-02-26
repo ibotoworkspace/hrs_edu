d0a928d4380800537c9207fb1b46f16969212bd7
Muhammed Hamza <hamzamuhammed404@gmail.com>
2021-02-26 15:10:02 +0500

    add quiz module

--- resources/views/admin/newquizquestion/partial/form.blade.php
+++ resources/views/admin/newquizquestion/partial/form.blade.php
@@ -1,85 +1,69 @@
 <div class="form-group">
     {{-- {!! dd($doctor->user->name) !!} --}}
-    {!! Form::label('name','Name') !!}
+    {!! Form::label('name', 'Name') !!}
     <div>
-        {!! Form::text('name', $doctor->user->name ?? null, ['class' => 'form-control',
-        'data-parsley-required'=>'true',
-        'data-parsley-trigger'=>'change',
-        'placeholder'=>'Name','required',
-        'maxlength'=>"100"]) !!}
+        {!! Form::text('name', $doctor->user->name ?? null, ['class' => 'form-control', 'data-parsley-required' => 'true', 'data-parsley-trigger' => 'change', 'placeholder' => 'Name', 'required', 'maxlength' => '100']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('email','Email') !!}
+        {!! Form::label('email', 'Email') !!}
         <div>
-            {!! Form::text('email',$doctor->user->email ?? null, ['class' => 'form-control',
-            'data-parsley-required'=>'true',
-            'data-parsley-trigger'=>'change',
-            'placeholder'=>'Email','required',
-            'maxlength'=>"100"]) !!}
+            {!! Form::text('email', $doctor->user->email ?? null, ['class' => 'form-control', 'data-parsley-required' => 'true', 'data-parsley-trigger' => 'change', 'placeholder' => 'Email', 'required', 'maxlength' => '100']) !!}
         </div>
         <div class="form-group">
-            {!! Form::label('age','Age') !!}
+            {!! Form::label('age', 'Age') !!}
             <div>
-                {!! Form::number('age',null, ['class' => 'form-control',
-                'data-parsley-required'=>'true',
-                'data-parsley-trigger'=>'change',
-                'placeholder'=>'Age','required',
-                'maxlength'=>"100"]) !!}
+                {!! Form::number('age', null, ['class' => 'form-control', 'data-parsley-required' => 'true', 'data-parsley-trigger' => 'change', 'placeholder' => 'Age', 'required', 'maxlength' => '100']) !!}
             </div>
 
         </div>
         <div class="form-group">
-            {!! Form::label('qualification','Qualification') !!}
+            {!! Form::label('qualification', 'Qualification') !!}
             <div>
-                {!! Form::text('qualification',null, ['class' => 'form-control',
-                'data-parsley-required'=>'true',
-                'data-parsley-trigger'=>'change',
-                'placeholder'=>'Qualification','required']) !!}
+                {!! Form::text('qualification', null, ['class' => 'form-control', 'data-parsley-required' => 'true', 'data-parsley-trigger' => 'change', 'placeholder' => 'Qualification', 'required']) !!}
             </div>
 
         </div>
 
         <div class="form-group">
-            {!! Form::label('doctor_type','Doctor Type',) !!}
-            {!! Form::select('doctor_type_id',$types,null,["placeholder"=>"Select
-            Type","class"=>"form-control","required"]) !!}
+            {!! Form::label('doctor_type', 'Doctor Type') !!}
+            {!! Form::select('doctor_type_id', $types, null, [
+    'placeholder' => "Select
+            Type",
+    'class' => 'form-control',
+    'required',
+]) !!}
             </select>
         </div>
 
 
         <div class="form-group">
-            {!! Form::label('total_duties','Total Duties') !!}
+            {!! Form::label('total_duties', 'Total Duties') !!}
             <div>
-                {!! Form::number('total_duties',null, ['class' => 'form-control',
-                'data-parsley-required'=>'true',
-                'data-parsley-trigger'=>'change',
-                'placeholder'=>'Total Duties','required']) !!}
+                {!! Form::number('total_duties', null, ['class' => 'form-control', 'data-parsley-required' => 'true', 'data-parsley-trigger' => 'change', 'placeholder' => 'Total Duties', 'required']) !!}
             </div>
 
         </div>
 
         <?php
+        $avatar = asset('images/medicallogo.png');
 
-$avatar =  asset('images/medicallogo.png');
-
-if(isset($user)){
-
-    if($user->avatar){
+        if (isset($user)) {
+        if ($user->avatar) {
         $avatar = $user->avatar;
-    }
-}
-?>
+        }
+        }
+        ?>
 
         <div class="form-group">
 
             <div class="form-group pull-right">
-                <img width="100px" src="{!! $avatar !!}"class="show-product-img imgshow">
+                <img width="100px" src="{!! $avatar !!}" class="show-product-img imgshow">
             </div>
 
             <div class="form-group">
-                {!! Form::label('avatar','Image') !!}
-                {!! Form::file('avatar', ['class' => 'choose-image', 'id'=>'avatar'] ) !!}
+                {!! Form::label('avatar', 'Image') !!}
+                {!! Form::file('avatar', ['class' => 'choose-image', 'id' => 'avatar']) !!}
                 <p class="help-block" id="error">Limit 2MB</p>
             </div>
 
@@ -99,18 +83,50 @@ if(isset($user)){
         <div class="col-md-5 pull-left">
             <div class="form-group text-center">
                 <div>
-                    {!!Form::submit('Save',
-                    ['class'=>'btn btn-primary btn-block btn-lg btn-parsley','onblur'=>'return validateForm();'])!!}
+                    {!! Form::submit('Save', ['class' => 'btn btn-primary btn-block btn-lg btn-parsley', 'onblur' => 'return validateForm();']) !!}
                 </div>
             </div>
         </div>
 
-        @section('app_jquery')
-        <script>
-            function validateForm(){
-        return true;
-    }
 
-        </script>
+
+        @section('app_jquery')
+            <script>
+                function validateForm() {
+                    return true;
+                }
+
+                function addcoice() {
+                    console.log('sfkhgyjutftjgfjg')
+
+                    var nextdivnum = $('.add').length + 1;
+                    console.log('sfdffff', nextdivnum)
+                    $('.radioBtn').append(radioBtnHtml(nextdivnum));
+                }
+
+                function radioBtnHtml(nextdivnum) {
+                    return `<div class="dummy">
+                <lable>Choice</lable>
+                <input type="text" class="add form-control" name="` + nextdivnum + `" style="margin-top: 10px; margin-bottom: 5px;">
+                </div>
+            `
+                }
+
+                function removeChoice() {
+                    console.log('length', $('.dummy').length);
+                    if ($('.dummy').length < 2) {
+                        return;
+                    }
+                    $('.dummy:last').remove();
+                }
+
+            </script>
+
+            <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+
+
+
+
+
 
         @endsection
