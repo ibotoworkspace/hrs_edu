57f7309779ff2e98b626bbb7cc39469ff8ed713c
Muhammed Hamza <hamzamuhammed404@gmail.com>
2021-03-09 15:30:35 +0500

    update home controller

--- app/Http/Controllers/services/HomeController.php
+++ app/Http/Controllers/services/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\services;
 
 use App\Http\Controllers\Controller;
 use App\Models\Courses;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 
@@ -13,9 +14,13 @@ class HomeController extends Controller
     {
 
         try {
+            $header = $request->header('authorization-secure') ?? $request->header('Authorization-secure');
+            $user = User::where('access_token', $header)->first();
             $items_count = $request->items_count ?? '10';
             $search = $request->course_name ?? '';
-            $courses=Courses::where('title', 'like', '%' . $search . '%')->with('registerCourse')->orderBy('created_at','desc')->paginate($items_count);
+            $courses=Courses::where('title', 'like', '%' . $search . '%')->with('registerCourse',function($q) use($user){
+                $q->where('user_id',$user->id);
+            })->orderBy('created_at','desc')->paginate($items_count);
 
             return $this->sendResponse(200, $courses);
             
