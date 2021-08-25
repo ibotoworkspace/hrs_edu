929b8db8f85bc89151920b934f32f21e50961466
hassan mirza <72553949+hassan0342@users.noreply.github.com>
2021-08-25 20:06:15 +0500

    fix

--- resources/views/admin/promocode/partial/form.blade.php
+++ resources/views/admin/promocode/partial/form.blade.php
@@ -141,7 +141,7 @@
  
         <div class="maininput">
          {{-- <div class="maininput"> --}}
-            {!! Form::date('validity', $new_date ??'', ['class' => 'form-control', 'data-parsley-required' => 'true', 'data-parsley-trigger' => 'change',  'required', 'maxlength' => '100']) !!}
+            {!! Form::date('validity', ['class' => 'form-control', 'data-parsley-required' => 'true', 'data-parsley-trigger' => 'change',  'required', 'maxlength' => '100']) !!}
     </div> 
         </div>
 
